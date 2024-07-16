d2203d27d98a9f3ee811b3fb3b5d7666d7df8113
duruo <duruo@dianjia.io>
2024-07-16 14:33:13 +0800

    add some info

--- apps/demo/src/App.tsx
+++ apps/demo/src/App.tsx
@@ -17,6 +17,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <Button />
+        <h1>jack</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
